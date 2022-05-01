713312e27ce5b4299fb921ecce51ec8e77450060
Kirill Ki <kirillkalapov@gmail.com>
2022-05-01 19:10:53 +0200

    Basic project + oboarding + tab bar

---
+++ NewsNews/Onboarding/OnboardingContainerViewControllerDelegate.swift
@@ -0,0 +1,169 @@
+//
+//  OnboardingContainerViewControllerDelegate.swift
+//  NewsNews
+//
+//  Created by  Mr.Ki on 01.05.2022.
+//
+
+import UIKit
+
+protocol OnboardingContainerViewControllerDelegate: AnyObject {
+    func didFinishOnboarding()
+}
+
+class OnboardingContainerViewController: UIViewController {
+
+    let pageViewController: UIPageViewController
+    var pages = [UIViewController]()
+    var currentVC: UIViewController
+ 
+    private let closeButton: UIButton = {
+        
+        let closeButton = UIButton(type: .system)
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.tintColor = .systemGray
+        closeButton.setTitle("Close", for: [])
+        closeButton.addTarget(self, action: #selector(closeTapped), for: .primaryActionTriggered)
+        return closeButton
+    }()
+    private let startButton: UIButton = {
+        
+        let button = UIButton(type: .system)
+        button.backgroundColor = .clear
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.gray.cgColor
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Get started", for: .normal)
+        button.setTitleColor(.gray, for: .normal)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 14
+        button.addTarget(self, action: #selector(atartTapped), for: .touchUpInside)
+        return button
+    }()
+    weak var delegate: OnboardingContainerViewControllerDelegate?
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        
+        let page1 = OnboardingViewController(heroImageName: "11", titleText: "Hey, this is news app 👋")
+        let page2 = OnboardingViewController(heroImageName: "22", titleText: "Nothing extra, just news 🍩")
+        let page3 = OnboardingViewController(heroImageName: "33", titleText: "Let's start 🚀")
+        
+        pages.append(page1)
+        pages.append(page2)
+        pages.append(page3)
+        
+        currentVC = pages.first!
+        
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+     //   style()
+        layout()
+        
+    }
+    
+    private func setup() {
+        view.backgroundColor = .systemGray5
+        //MARK: - Load and sutup pageVC
+        addChild(pageViewController)
+        view.addSubview(pageViewController.view)
+        view.addSubview(closeButton)
+        view.addSubview(startButton)
+ 
+        pageViewController.didMove(toParent: self)
+        
+        //MARK: - Delegate
+        pageViewController.dataSource = self
+        //MARK: - Turn off auto constaraints
+        
+        pageViewController.view.translatesAutoresizingMaskIntoConstraints = false
+
+        pageViewController.setViewControllers([pages.first!], direction: .forward, animated: false, completion: nil)
+        currentVC = pages.first!
+    }
+    
+    
+    private func layout() {
+        
+        let pageViewControllerConstraints = [
+            view.topAnchor.constraint(equalTo: pageViewController.view.topAnchor),
+            view.leadingAnchor.constraint(equalTo: pageViewController.view.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: pageViewController.view.trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: pageViewController.view.bottomAnchor),
+        ]
+        NSLayoutConstraint.activate(pageViewControllerConstraints)
+        
+        let closeButtonConstraints = [
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: closeButton.trailingAnchor, multiplier: 4),
+        //    closeButton.trailingAnchor.constraint(equalToSystemSpacingAfter: view.trailingAnchor, multiplier: 2),
+            closeButton.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 4)
+        ]
+        NSLayoutConstraint.activate(closeButtonConstraints)
+        
+        let startButtonConstraints = [
+            startButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            startButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            startButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -150)
+           
+        ]
+        NSLayoutConstraint.activate(startButtonConstraints)
+    
+    }
+    
+}
+
+// MARK: - UIPageViewControllerDataSource, swipe logic
+extension OnboardingContainerViewController: UIPageViewControllerDataSource {
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return getPreviousViewController(from: viewController)
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return getNextViewController(from: viewController)
+    }
+
+    private func getPreviousViewController(from viewController: UIViewController) -> UIViewController? {
+        guard let index = pages.firstIndex(of: viewController), index - 1 >= 0 else { return nil }
+        currentVC = pages[index - 1]
+        return pages[index - 1]
+    }
+
+    private func getNextViewController(from viewController: UIViewController) -> UIViewController? {
+        guard let index = pages.firstIndex(of: viewController), index + 1 < pages.count else { return nil }
+        currentVC = pages[index + 1]
+        return pages[index + 1]
+    }
+
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return pages.count
+    }
+
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return pages.firstIndex(of: self.currentVC) ?? 0
+    }
+}
+
+//MARK: - Actions
+
+extension OnboardingContainerViewController {
+    @objc func closeTapped(_ sender: UIButton) {
+        delegate?.didFinishOnboarding()
+        print("Close tapped")
+    }
+    @objc func atartTapped(_ sender: UIButton) {
+        delegate?.didFinishOnboarding()
+        print("Start tapped")
+    }
+}
+
+
