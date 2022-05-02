b49933b624083c82472f26d6c741a3eef5789ddb
Kirill Ki <kirillkalapov@gmail.com>
2022-05-02 14:58:52 +0200

    Add animation and move extensions

--- NewsNews/View/ListViewController.swift
+++ NewsNews/View/ListViewController.swift
@@ -6,12 +6,9 @@
 //
 
 import UIKit
-import SafariServices
 
 class ListViewController: UIViewController {
-    
- //   lazy var newsPresenter = NewsPresenter(newsService: NewsService(), newsView: self)
-    
+
     let cellId = "cell"
     var tableView = UITableView()
     var savedNewsToDisplay = [PostList]()
@@ -21,39 +18,18 @@ class ListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
         setup()
         NotificationCenter.default.addObserver(forName: NSNotification.Name("loaded"), object: nil, queue: nil) { _ in
             self.downloadNewsFromList()
         }
-      //  newsPresenter.getNews()
     }
-    
-//    override func loadView() {
-//        view = tableView
-//    }
-    
+
     func setup() {
         downloadNewsFromList()
         setupTableView()
         setupRefreshControl()
-        
-        
-        
     }
-    
-    private func downloadNewsFromList() {
-        CoreDataManager.shared.fetchNewsFromDataBase { [weak self] result in
-            switch result {
-            case .success(let news):
-                self?.savedNewsToDisplay = news
-                self?.tableView.reloadData()
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-    }
-    
+
     private func setupTableView() {
         
         tableView.dataSource = self
@@ -62,14 +38,7 @@ class ListViewController: UIViewController {
         tableView.backgroundColor = converter.hexStringToUIColor(hex: "#212529")
         tableView.separatorStyle = .none
         
-//        tableView.backgroundColor = appColor
-//        tableView.delegate = self
-//        tableView.dataSource = self
-        
-//        tableView.register(AccountSummaryCell.self, forCellReuseIdentifier: AccountSummaryCell.reuseID)
-//        tableView.register(SkeletonCell.self, forCellReuseIdentifier: SkeletonCell.reuseID)
-//        tableView.rowHeight = AccountSummaryCell.rowHeight
-        tableView.tableFooterView = UIView()
+      //  tableView.tableFooterView = UIView()
         
         tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
@@ -83,95 +52,3 @@ class ListViewController: UIViewController {
     }
     
 }
-
-
-extension ListViewController: UITableViewDataSource, UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: cellId)
-        cell.selectionStyle = .none
-        cell.backgroundColor = converter.hexStringToUIColor(hex: "#212529")
-        cell.textLabel?.textColor = converter.hexStringToUIColor(hex: "#95d5b2")
-        cell.detailTextLabel?.textColor = converter.hexStringToUIColor(hex: "#c77dff")
-        let newsViewData = savedNewsToDisplay[indexPath.row]
-        cell.textLabel?.text = newsViewData.title
-        cell.textLabel?.numberOfLines = 0
-        cell.detailTextLabel?.text = "◉\(String(newsViewData.points))"
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return savedNewsToDisplay.count
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let news = savedNewsToDisplay[indexPath.row]
-        guard let url = URL(string: news.url ?? "https://hn.algolia.com") else {return}
-        let configuration = SFSafariViewController.Configuration()
-        let safariViewController = SFSafariViewController(url: url, configuration: configuration)
-        safariViewController.modalPresentationStyle = .fullScreen
-        present(safariViewController, animated: true)
-        
-    }
-    
-    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        let item = UIContextualAction(style: .normal, title: "Delete") {  (contextualAction, view, boolValue) in
-               print("delete")
-            
-            CoreDataManager.shared.deleteNewsFromDataBase(model: self.savedNewsToDisplay[indexPath.row]) { [weak self] result in
-                switch result {
-                case .success():
-                    print("Deleted from database")
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-                self?.savedNewsToDisplay.remove(at: indexPath.row)
-                tableView.deleteRows(at: [indexPath], with: .fade)
-            }
-            
-            }
-            item.image = UIImage(systemName: "trash")
-            item.backgroundColor = converter.hexStringToUIColor(hex: "#95d5b2")
-        
-
-            let swipeActions = UISwipeActionsConfiguration(actions: [item])
-        
-            return swipeActions
-        }
-    
-}
-
-extension ListViewController: SavedNewsView {
-    func setNews(news: [PostList]) {
-        DispatchQueue.main.async {
-            self.savedNewsToDisplay = news
-            self.tableView.reloadData()
-        }
-    }
-    
-    func setEmptyNews() {
-        view.backgroundColor = .systemRed
-    }
-    
-    
-}
-
-extension ListViewController {
-    
-    private func setupRefreshControl() {
-        //  refreshControl.tintColor = appColor
-        refreshControl.addTarget(self, action: #selector(refreshContent), for: .valueChanged)
-        refreshControl.tintColor = converter.hexStringToUIColor(hex: "#c77dff")
-        tableView.refreshControl = refreshControl
-    }
-    @objc func refreshContent() {
-        tableView.reloadData()
-        print("refresh")
-        let dispatchTime = DispatchTime.now() + Double(0.5)
-        DispatchQueue.main.asyncAfter(deadline: dispatchTime, execute: {
-            self.tableView.refreshControl?.endRefreshing()
-        })
-    }
-}
-
--- NewsNews/View/MainViewController.swift
+++ NewsNews/View/MainViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import SafariServices
 
 class MainViewController: UIViewController {
     
@@ -17,6 +16,7 @@ class MainViewController: UIViewController {
     var newsToDisplay = [Post]()
     let refreshControl = UIRefreshControl()
     let converter = ColorConverter()
+    let animation = Animation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,15 +26,9 @@ class MainViewController: UIViewController {
         newsPresenter.getNews()
     }
     
-//    override func loadView() {
-//        view = tableView
-//    }
-    
     func setup() {
         setupTableView()
         setupRefreshControl()
-        
-        
         
     }
     
@@ -45,16 +39,7 @@ class MainViewController: UIViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
         tableView.backgroundColor = converter.hexStringToUIColor(hex: "#212529")
         tableView.separatorStyle = .none
-        
-//        tableView.backgroundColor = appColor
-//        tableView.delegate = self
-//        tableView.dataSource = self
-        
-//        tableView.register(AccountSummaryCell.self, forCellReuseIdentifier: AccountSummaryCell.reuseID)
-//        tableView.register(SkeletonCell.self, forCellReuseIdentifier: SkeletonCell.reuseID)
-//        tableView.rowHeight = AccountSummaryCell.rowHeight
-        tableView.tableFooterView = UIView()
-        
+      //  tableView.tableFooterView = UIView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
         
@@ -65,114 +50,5 @@ class MainViewController: UIViewController {
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
-    
 }
 
-
-extension MainViewController: UITableViewDataSource, UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: cellId)
-        cell.selectionStyle = .none
-        cell.backgroundColor = converter.hexStringToUIColor(hex: "#212529")
-        cell.textLabel?.textColor = converter.hexStringToUIColor(hex: "#95d5b2")
-        cell.detailTextLabel?.textColor = converter.hexStringToUIColor(hex: "#c77dff")
-        let newsViewData = newsToDisplay[indexPath.row]
-        cell.textLabel?.text = newsViewData.title
-        cell.textLabel?.numberOfLines = 0
-        cell.detailTextLabel?.text = "◉\(String(newsViewData.points))"
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return newsToDisplay.count
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let news = newsToDisplay[indexPath.row]
-        guard let url = URL(string: news.url ?? "https://hn.algolia.com") else {return}
-        let configuration = SFSafariViewController.Configuration()
-        let safariViewController = SFSafariViewController(url: url, configuration: configuration)
-        safariViewController.modalPresentationStyle = .fullScreen
-        present(safariViewController, animated: true)
-        
-    }
-    
-    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        let item = UIContextualAction(style: .normal, title: "Add to list") {  (contextualAction, view, boolValue) in
-               print("Paperclip tapped")
-            self.addNewsToList(indexPath: indexPath)
-            
-            }
-            item.image = UIImage(systemName: "paperclip")
-            item.backgroundColor = converter.hexStringToUIColor(hex: "#95d5b2")
-        
-
-            let swipeActions = UISwipeActionsConfiguration(actions: [item])
-        
-            return swipeActions
-        }
-    
-}
-
-extension MainViewController: NewsView {
-    func setNews(news: [Post]) {
-        DispatchQueue.main.async {
-            self.newsToDisplay = news
-            self.tableView.reloadData()
-        }
-    }
-    
-    func setEmptyNews() {
-        view.backgroundColor = .systemRed
-    }
-    
-    
-}
-
-extension MainViewController {
-    
-    private func setupRefreshControl() {
-        //  refreshControl.tintColor = appColor
-        refreshControl.addTarget(self, action: #selector(refreshContent), for: .valueChanged)
-        refreshControl.tintColor = converter.hexStringToUIColor(hex: "#c77dff")
-        tableView.refreshControl = refreshControl
-    }
-    @objc func refreshContent() {
-        tableView.reloadData()
-        print("refresh")
-        let dispatchTime = DispatchTime.now() + Double(0.5)
-        DispatchQueue.main.asyncAfter(deadline: dispatchTime, execute: {
-            self.tableView.refreshControl?.endRefreshing()
-        })
-    }
-}
-
-extension MainViewController {
-    
-    private func addNewsToList(indexPath: IndexPath) {
-        
-        CoreDataManager.shared.downloadNewsToDataBase(model: newsToDisplay[indexPath.row]) { result in
-            switch result {
-            case .success():
-                print("Downloaded to DB")
-                NotificationCenter.default.post(name: NSNotification.Name("loaded"), object: nil)
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-        
-//        DataPersistentManager.shared.downloadMovieToDataBase(model: movies[indexPath.row]) { result in
-//            switch result {
-//            case .success():
-//                print("Downloaded to DB")
-//                NotificationCenter.default.post(name: NSNotification.Name("loaded"), object: nil)
-//            case .failure(let error):
-//                print(error.localizedDescription)
-//            }
-//        }
-//     //   print("Downloading \(movies[indexPath.row].title)")
-    }
-    
-}
